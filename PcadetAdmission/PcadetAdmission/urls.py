254dc031e8e96a0959ee9c9d99b4e8cedacae924
pex <pexist@gmail.com>
2020-12-08 17:00:58 +0700

    basic student ready

--- PcadetAdmission/PcadetAdmission/urls.py
+++ PcadetAdmission/PcadetAdmission/urls.py
@@ -15,6 +15,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+
 from admissionlogin import views as lg
 from admissDetail import views as ad
 
